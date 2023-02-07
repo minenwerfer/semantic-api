17fc508d92aa3689b2291c09216e43e084ce1cbb
ringeringeraja <joaosan177@gmail.com>
2023-02-07 01:30:05 -0300

    recursive formatvalue

---
+++ packages/common/formatValue.ts
@@ -0,0 +1,34 @@
+import type { CollectionProperty } from '../types'
+
+export const formatValue = (
+  value: any,
+  key: string,
+  property?: CollectionProperty,
+  index?: string
+): string => {
+  if( Array.isArray(value) ) {
+    return value.map(v => formatValue(v, key, property)).join(', ')
+  }
+
+  const firstValue = (() => {
+    if( property?.s$isReference ) {
+      const firstIndex = index || property.s$indexes![0]
+      return value[firstIndex]
+    }
+
+    return value
+  })()
+
+  const formatted = (() => {
+    switch(true) {
+      case ['date', 'date-time'].includes(property?.format!):
+        return String(firstValue).formatDateTime(property?.format === 'date-time')
+
+      case property?.type === 'boolean': return firstValue ? 'true' : 'false'
+      case [undefined, null].includes(firstValue): return '-'
+      default: return firstValue
+    }
+  })()
+
+  return String(formatted)
+}
