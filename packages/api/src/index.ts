2fb81c15e37bb569fa996a5808e47e97adab4767
ringeringeraja <joaosan177@gmail.com>
2023-05-07 21:13:24 -0300

    tidy up

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,7 +1,6 @@
 export * from './src/assets'
 export * from './src/collection'
 export * from './src/exceptions'
-export * from './src/accessControl'
 export * from './src/token'
 export * from './src/render'
 export * from './src/types'
