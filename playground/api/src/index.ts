321ab2b61c5db9c420aac3d947242e0b93932844
ringeringeraja <joaosan177@gmail.com>
2023-07-15 02:56:06 -0300

    update playground

--- playground/api/src/index.ts
+++ playground/api/src/index.ts
@@ -1,50 +1,13 @@
 import { initWithDatabase } from '@semantic-api/server'
-import { useFunctions } from '@semantic-api/api'
 import type { AccessControl } from '@semantic-api/access-control'
 
 import person from './person'
 import pet from './pet'
 import algorithm from './algorithm'
 
-const { getAll, insert } = useFunctions<any, any>()
-
 export const collections = {
   person,
-  pet,
-  car: () => ({
-    item: {},
-    description: <const>{
-      $id: 'car',
-      strict: true,
-      properties: {
-        name: {
-          type: 'string'
-        },
-        brand: {
-          enum: [
-            'vw',
-            'fiat',
-            'ford',
-            'mercedes'
-          ]
-        }
-      },
-    },
-    functions: {
-      getAll,
-      insert,
-      test: async (_: null, context: Context<any>) => {
-        await context.collection.functions.insert({
-          what: {
-            name: 'gol',
-            brand: context.description.properties.brand.enum[Math.floor(Math.random()*context.description.properties.brand.enum.length)]
-          }
-        }, context)
-
-        return context.collection.functions.getAll({}, context)
-      }
-    }
-  })
+  pet
 }
 
 export const algorithms = {
@@ -59,9 +22,6 @@ export const accessControl: AccessControl<Collections, Algorithms> = {
           functions: [
             'getAll'
           ]
-        },
-        car: {
-          grantEverything: true
         },
         algorithm: {
           functions: [
